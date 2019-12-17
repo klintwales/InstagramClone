ebd6e72dbb05a00213b1826dab6b6a40a5b592bb
klintwales <51382788+Klint@users.noreply.github.com>
2019-12-16 19:28:47 -0600

    Added Hello World and Hi World zoom in and out.

--- AnimationsInAndroid/app/src/main/java/com/example/animationsinandroid/MainActivity.java
+++ AnimationsInAndroid/app/src/main/java/com/example/animationsinandroid/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtHelloWorld;
     private TextView txtHiWorld;
+    private TextView txtAndroid;
+    private Button btnAnimate;
 
     private boolean isHelloWorldShowing;
 
@@ -20,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txtHelloWorld = findViewById(R.id.txtHelloWorld);
         txtHiWorld = findViewById(R.id.txtHiWorld);
+        txtAndroid = findViewById(R.id.txtAndroid);
+        btnAnimate = findViewById(R.id.btnAnimate);
+
+        txtHelloWorld.setX(2000);
+        txtAndroid.setX(-2000);
 
         txtHelloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        txtAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtAndroid.animate().translationXBy(200).setDuration(5000);
+            }
+        });
+
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHelloWorld.animate().translationXBy(-2000);
+                txtAndroid.animate().translationXBy(2000);
+            }
+        });
+
+
 
     }
 }
