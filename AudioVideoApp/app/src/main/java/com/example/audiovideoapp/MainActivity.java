0c07708b5770f031f125ce8e94a1503279fb7613
klintwales <51382788+Klint@users.noreply.github.com>
2020-01-01 18:58:57 -0600

    Added quack to res folder. Made previous blank buttons play and pause quack. Added seek bars for volume and duration of quack.

--- AudioVideoApp/app/src/main/java/com/example/audiovideoapp/MainActivity.java
+++ AudioVideoApp/app/src/main/java/com/example/audiovideoapp/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.audiovideoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, MediaPlayer.OnCompletionListener{
 
     private VideoView myVideoView;
     private Button btnPlayVideo;
+    private Button btnPlayMusic, btnPauseMusic;
+    private MediaPlayer mediaPlayer;
+    private SeekBar volumeSeekBar;
+    private SeekBar moveBackAndForthSeekBar;
+    private Timer timer;
+
     private MediaController mediaController;
+    private AudioManager audioManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +37,113 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         myVideoView = findViewById(R.id.myVideoView);
         btnPlayVideo = findViewById(R.id.btnPlayVideo);
+
+        btnPlayMusic = findViewById(R.id.btnPlayMusic);
+        btnPauseMusic = findViewById(R.id.btnPauseMusic);
+        volumeSeekBar = findViewById(R.id.seekBarVolume);
+        moveBackAndForthSeekBar = findViewById(R.id.seekBarMove);
+
+
+
         btnPlayVideo.setOnClickListener(MainActivity.this);
+        btnPlayMusic.setOnClickListener(MainActivity.this);
+        btnPauseMusic.setOnClickListener(MainActivity.this);
+
+
 
         mediaController = new MediaController(MainActivity.this);
+        mediaPlayer = mediaPlayer.create(this, R.raw.quack);
+        audioManager = ( AudioManager ) getSystemService(AUDIO_SERVICE);
+
+        int maximumVolumeOfUserDevice = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolumeOfUserDevice = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        volumeSeekBar.setMax(maximumVolumeOfUserDevice);
+        volumeSeekBar.setProgress(currentVolumeOfUserDevice);
+
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    //Toast.makeText(MainActivity.this, Integer.toString(progress), Toast.LENGTH_LONG).show();
+                    audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        moveBackAndForthSeekBar.setOnSeekBarChangeListener(MainActivity.this);
+        moveBackAndForthSeekBar.setMax(mediaPlayer.getDuration());
+
+        mediaPlayer.setOnCompletionListener(this);
+
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View buttonView) {
 
-        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
-        myVideoView.setVideoURI(videoUri);
+        switch (buttonView.getId()){
 
-        myVideoView.setMediaController(mediaController);
-        mediaController.setAnchorView(myVideoView);
+            case R.id.btnPlayVideo:
+                Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video);
+                myVideoView.setVideoURI(videoUri);
 
-        myVideoView.start();
+                myVideoView.setMediaController(mediaController);
+                mediaController.setAnchorView(myVideoView);
+
+                myVideoView.start();
+                timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                       moveBackAndForthSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                },0, 1000);
+
+
+                break;
+            case R.id.btnPlayMusic:
+                mediaPlayer.start();
+                break;
+            case R.id.btnPauseMusic:
+                mediaPlayer.pause();
+                timer.cancel();
+                break;
+
+        }
+
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (fromUser){
+            Toast.makeText(this, Integer.toString(progress), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        timer.cancel();
+        Toast.makeText(this, "music has ended", Toast.LENGTH_SHORT).show();
     }
 }
